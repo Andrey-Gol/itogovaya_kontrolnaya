04aed98cb95cd2dac7869c7e481e7023310b8476
Andrey <nedogeograf@gmail.com>
2024-11-26 19:50:44 +0300

    dopisal programmu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿
-
+﻿string [] array = {"await", "bb", "ccc"};
+if (array.Length == 0)
+{
+    array = Console.ReadLine()!.Split(" ");
+}
+string [] new_array = array;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+        Console.Write("{0} ", new_array[i]);
+    }
+}
